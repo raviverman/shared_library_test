c4b2ede96bef42d5b052e5fdfcd0eef3fc573e19
Ravi Verman <raviverman@hotmail.com>
2023-10-23 19:55:05 +0000

    Added initial commit with src and lib files

---
+++ src/dyn_math.c
@@ -0,0 +1,3 @@
+int add_stuff(int a, int b) {
+    return a + b;
+}
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "math_fun.h"
+
+int main( int argc, char ** argv ) {
+   int a=5, b=10;
+   printf( "Got Sum: %d\n", add_stuff( a, b ) );
+   return 0;
+}
